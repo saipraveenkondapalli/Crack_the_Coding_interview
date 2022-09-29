26f59ab729914146757e1e7d65f4ead2d5ca34a9
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2022-09-29 20:32:21 +0530

    added problems

--- recursion and DP/8.5.py
+++ recursion and DP/8.5.py
@@ -1,16 +1,27 @@
-
-def multiply(m,n):
-    n = max(m,n)
-    m = min(m,n)
-    return multiply_helper(n,m)
+"""
+Multipy two numbers without using * or / operator.
+"""
 
 
-def multiply_helper(n,m):
-    if m == 0:return 0
-    return n+multiply_helper(n,m-1)
+def multipy2(m, n):
+    big = max(m, n)
+    small = min(m, n)
+    return multiply_helper2(big, small)
+
+
+def multiply_helper2(big, small):
+    # Runtime: O(log m) where m is the smaller number because we are dividing it by 2 each time until it reaches base case
+    if small == 0: return 0 # base case
+    if small == 1: return big # base case
+    s = small >> 1  # divide by 2 (right shift)  Example: 5 >> 1 = 2 i.e 5(101) >> 1 = ( 1 0 )
+    halfProduct = multiply_helper2(big, s)
+    if small % 2 == 0:  # even
+        return halfProduct + halfProduct  # double it
+    else:
+        return halfProduct + halfProduct + big  # double it and add the big number to it
 
 
 if __name__ == "__main__":
+    print(multipy2(31,35)) # 1085
 
-    print(multiply(6,500))
 
