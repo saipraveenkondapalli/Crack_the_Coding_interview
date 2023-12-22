92b13f79f74c0fa8bc3fa268cf166320fc8c41d3
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2023-12-22 18:52:14 +0530

    Remove example in the description

--- tree/4.6.py
+++ tree/4.6.py
@@ -2,18 +2,6 @@
 Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
 binary search tree. You may assume that each node has a link to its parent.
 
-Example
-Input:  node 3 from tree below
-Output: 4
-
-        2
-      /   \
-     1     5
-          / \
-         4   6
-            /
-           3
-
 """
 
 from tree import Tree, TreeNode
