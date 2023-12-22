ea6710120c9d86ffcb2b8ded2f8d28e021b8f440
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2023-12-22 18:27:05 +0530

    Update import statement and class name in 4.5.py

--- tree/4.5.py
+++ tree/4.5.py
@@ -3,7 +3,7 @@ Check if a given binary tree is a valid binary search tree.
 
 """
 
-from tree_linked_in import Tree, Node
+from tree import Tree, TreeNode
 
 
 def Check_BST1(root):
@@ -11,7 +11,7 @@ def Check_BST1(root):
         return True
     if root.left and root.left.data >= root.data:
         return False
-    if root.right and root.right.data <= root.data:
+    if root.right and root.right.data < root.data:
         return False
     if not Check_BST1(root.left) or not Check_BST1(root.right): # same as if Check_BST1(root.left) == False or Check_BST1(root.right) == False:
 
@@ -21,10 +21,10 @@ def Check_BST1(root):
 
 if __name__ == "__main__":
     tree = Tree()
-    tree.root = Node(40)
-    tree.root.left = Node(30)
-    tree.root.right = Node(50)
-    tree.root.left.left = Node(25)
-    tree.root.left.right = Node(36)
-    tree.print()
+    tree.root = TreeNode(40)
+    tree.root.left = TreeNode(30)
+    tree.root.right = TreeNode(50)
+    tree.root.left.left = TreeNode(25)
+    tree.root.left.right = TreeNode(36)
+    
     print(Check_BST1(tree.root))
