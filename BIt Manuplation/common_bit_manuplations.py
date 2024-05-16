95d48e6f002824ee9cc68524b64a3a43405f952f
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-05-16 20:29:08 +0530

    update fun names

--- BIt Manuplation/common_bit_manuplations.py
+++ BIt Manuplation/common_bit_manuplations.py
@@ -1,26 +1,26 @@
 
 # get Bit
 
-def getBit(num, i):
+def get_bit(num, i):
     return (num & (1 << i)) != 0
 
 
-def setBit(num, i):
+def set_bit(num, i):
     return num | (1 << i)
 
 
-def clearBit(num, i):
+def clear_bit(num, i):
     return num & ~(1 << i)
 
 
-def updateBit(num, i, v):
+def update_bit(num, i, v):
     val = 1 if v else 0
     mask = ~(1 << i)
     return (num & mask) | (val << i)
 
 
 if __name__ == "__main__":
-    print(getBit(5, 2))  # 5 = 101, 1<<2 = 100 , 101 & 100 = 100, 100 != 0, True
-    print(setBit(5,1))  # 5 = 101, 1<<1 = 010, 101 | 010 = 111, 7
-    print(clearBit(5,2))  # 5 = 101, 1<<2 = 100, 101 & ~100 = 001, 1
-    print(updateBit(5,1,1))  # 5 = 101, 1<<1 = 010, 101 & ~010 = 101, 101 | 010 = 111, 7
+    print(get_bit(5, 2))  # 5 = 101, 1<<2 = 100 , 101 & 100 = 100, 100 != 0, True
+    print(set_bit(5, 1))  # 5 = 101, 1<<1 = 010, 101 | 010 = 111, 7
+    print(clear_bit(5, 2))  # 5 = 101, 1<<2 = 100, 101 & ~100 = 001, 1
+    print(update_bit(5, 1, 1))  # 5 = 101, 1<<1 = 010, 101 & ~010 = 101, 101 | 010 = 111, 7
