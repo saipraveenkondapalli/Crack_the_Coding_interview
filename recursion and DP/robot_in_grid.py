d2303ffc8ab17907efa8a00683c5e54804a6acec
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-09-30 12:19:54 +0530

    added doc and reformatted the file

--- recursion and DP/robot_in_grid.py
+++ recursion and DP/robot_in_grid.py
@@ -5,10 +5,17 @@ such that the robot cannot step on them.
 """
 
 
-# Solution 1: Runtime O(2^(r+c)) Space O(r+c), where r is the number of rows and c is the number of columns
-# This is because we are making 2 recursive calls for each cell in the grid i.e. 2^(r+c)
-# The space complexity is O(r+c) because we are using a list to store the path
+# Solution 1: This solution uses recursion and memoization with a set to keep track of failed points. The runtime
+# complexity is O(2^(r+c)) and the space complexity is O(r+c), where r is the number of rows and c is the number of
+# columns.
+
 def robot_in_a_grid(grid):
+    """
+    This function finds a path for the robot in the grid.
+
+    :param grid: 2D list representing the grid
+    :return: List of tuples representing the path if one exists, False otherwise
+    """
     if not grid or not grid[0]:
         return False
     path = []
@@ -19,21 +26,43 @@ def robot_in_a_grid(grid):
 
 
 def get_path(grid, row, col, path, failed_points):
+    """
+    This helper function uses recursion to find a path.
+
+    :param grid: 2D list representing the grid
+    :param row: Current row
+    :param col: Current column
+    :param path: List to store the path
+    :param failed_points: Set to store the points where a path could not be found
+    :return: True if a path is found, False otherwise
+    """
     if col < 0 or row < 0 or not grid[row][col]:
         return False
-    if (row, col) in failed_points: return False
 
-    is_at_origin = (row == 0) and (col == 0)
+    if (row, col) in failed_points:
+        return False
+
+    is_at_origin = (row == 0) and (col == 0)  # base case , we are at the origin
+
+    # if we are at the origin, or we can reach the origin from the current cell
     if is_at_origin or get_path(grid, row, col - 1, path, failed_points) or get_path(grid, row - 1, col, path,
                                                                                      failed_points):
         path.append((row, col))
         return True
+
+    # if we can't reach the origin from the current cell, we add the cell to the failed points
     failed_points.add((row, col))
     return False
 
 
-# optimized, Solution 2
+# Optimized Solution 2: This solution also uses recursion, but with a dictionary for memoization.
 def robot_in_a_grid_optimized(maze):
+    """
+    This function finds a path for the robot in the maze.
+
+    :param maze: 2D list representing the maze
+    :return: List of tuples representing the path if one exists, False otherwise
+    """
     if not maze or not maze[0]:
         return False
     path = []
@@ -44,26 +73,40 @@ def robot_in_a_grid_optimized(maze):
 
 
 def get_path_optimized(maze, row, col, path, cache):
-    if col < 0 or row < 0 or not maze[row][col]:
+    """
+    This helper function uses recursion to find a path and memoizes the results.
+
+    :param maze: 2D list representing the maze
+    :param row: Current row
+    :param col: Current column
+    :param path: List to store the path
+    :param cache: Dictionary to store the results of previous computations
+    :return: True if a path is found, False otherwise
+    """
+    if col < 0 or row < 0 or not maze[row][col]:  # if we are out of bounds, or we hit a wall
         return False
+
     p = (row, col)
-    if p in cache:
+    if p in cache:  # if we have already visited this cell
         return cache[p]
+
     is_at_origin = (row == 0) and (col == 0)
     if is_at_origin or get_path_optimized(maze, row, col - 1, path, cache) or get_path_optimized(maze, row - 1, col,
                                                                                                  path, cache):
         path.append(p)
-        cache[p] = True
+        cache[p] = True  # if we can reach the origin from the current cell, we add the cell to the cache
         return True
-    cache[p] = False
+
+    cache[p] = False  # if we can't reach the origin from the current cell, we add the cell to the cache
+
     return False
 
 
 if __name__ == "__main__":
-    g = [[True, True, True, True, True, False],         # - - - - - X
-         [False, False, False, False, True, False],     # X X X X - X
-         [True, True, True, True, True, False],         # X X X X - X
-         [True, False, False, False, True, False],      # X X X X - X
-         [True, True, True, True, True, True]]          # X X X X - -
+    g = [[True, True, True, True, True, False],  # - - - - - X
+         [False, False, False, False, True, False],  # X X X X - X
+         [True, True, True, True, True, False],  # X X X X - X
+         [True, False, False, False, True, False],  # X X X X - X
+         [True, True, True, True, True, True]]  # X X X X - -
     print(robot_in_a_grid(g))
     print(robot_in_a_grid_optimized(g))
