171657eda69ca4ab7c9948257f846395ae453dcf
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-09-30 12:13:02 +0530

    reformat file

--- recursion and DP/triple_steps.py
+++ recursion and DP/triple_steps.py
@@ -2,7 +2,6 @@
 Triple Steps: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time.
 Implement a method to count how many possible ways the child can run up the stairs.
 """
-import unittest
 
 
 def triple_steps(n):
@@ -11,24 +10,27 @@ def triple_steps(n):
     elif n == 0:
         return 1
     else:
-        return triple_steps(n-1) + triple_steps(n-2) + triple_steps(n-3)
+        return triple_steps(n - 1) + triple_steps(n - 2) + triple_steps(n - 3)
 
 
 # Memoization
 def triple_step_optimized(n):
-    memo = [-1] * (n+1)
+    memo = [-1] * (n + 1)
     return triple_steps_optimized_helper(n, memo)
 
 
 def triple_steps_optimized_helper(n, memo):
-    if n < 0: return 0
-    elif n == 0: return 1
-    if memo[n] != -1 : return memo[n]
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    if memo[n] != -1:
+        return memo[n]
     else:
-        return triple_steps_optimized_helper(n-1,memo) + triple_steps_optimized_helper(n-2,memo) + triple_steps_optimized_helper(n-3, memo)
-
+        return (triple_steps_optimized_helper(n - 1, memo) + triple_steps_optimized_helper(n - 2, memo) +
+                triple_steps_optimized_helper(n - 3, memo))
 
 
 if __name__ == "__main__":
-    #unittest.main()
-    print(hash(2))
+    print(triple_steps(3))  # 4
+    print(triple_step_optimized(3))  # 4
