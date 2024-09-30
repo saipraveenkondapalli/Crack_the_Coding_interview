826025413dafcd3b401ca80892f370a30b7d94bb
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-09-30 12:13:22 +0530

    use set for failed points instead of list

--- recursion and DP/robot_in_grid.py
+++ recursion and DP/robot_in_grid.py
@@ -1,5 +1,5 @@
 """
-Robot in a Grid: Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+Robot in a Grid: Imagine a robot sitting in the upper left corner of grid with r rows and c columns.
 The robot can only move in two directions, right and down, but certain cells are "off limits"
 such that the robot cannot step on them.
 """
@@ -12,8 +12,8 @@ def robot_in_a_grid(grid):
     if not grid or not grid[0]:
         return False
     path = []
-    failed_points = []
-    if get_path(grid, len(grid)-1, len(grid[0])-1, path, failed_points):
+    failed_points = set()
+    if get_path(grid, len(grid) - 1, len(grid[0]) - 1, path, failed_points):
         return path
     return False
 
@@ -24,10 +24,11 @@ def get_path(grid, row, col, path, failed_points):
     if (row, col) in failed_points: return False
 
     is_at_origin = (row == 0) and (col == 0)
-    if is_at_origin or get_path(grid, row, col-1, path,failed_points) or get_path(grid, row-1, col, path, failed_points):
+    if is_at_origin or get_path(grid, row, col - 1, path, failed_points) or get_path(grid, row - 1, col, path,
+                                                                                     failed_points):
         path.append((row, col))
         return True
-    failed_points.append((row, col))
+    failed_points.add((row, col))
     return False
 
 
@@ -37,19 +38,20 @@ def robot_in_a_grid_optimized(maze):
         return False
     path = []
     cache = {}
-    if get_path_optimized(maze, len(maze)-1, len(maze[0])-1, path,cache):
+    if get_path_optimized(maze, len(maze) - 1, len(maze[0]) - 1, path, cache):
         return path
     return False
 
 
-def get_path_optimized(maze,row,col,path,cache):
-    if col<0 or row<0 or not maze[row][col]:
+def get_path_optimized(maze, row, col, path, cache):
+    if col < 0 or row < 0 or not maze[row][col]:
         return False
     p = (row, col)
     if p in cache:
         return cache[p]
     is_at_origin = (row == 0) and (col == 0)
-    if is_at_origin or get_path_optimized(maze, row, col-1, path,cache) or get_path_optimized(maze, row-1, col, path,cache):
+    if is_at_origin or get_path_optimized(maze, row, col - 1, path, cache) or get_path_optimized(maze, row - 1, col,
+                                                                                                 path, cache):
         path.append(p)
         cache[p] = True
         return True
@@ -58,10 +60,10 @@ def get_path_optimized(maze,row,col,path,cache):
 
 
 if __name__ == "__main__":
-    grid = [[True, True, True, True, True, False],       # - - - - - X
-            [False, False, False, False, True, False],   # X X X X - X
-            [True, True, True, True, True, False],       # X X X X - X
-            [True, False, False, False, True, False],    # X X X X - X
-            [True, True, True, True, True, True]]        # X X X X - -
-    print(robot_in_a_grid(grid))
-    print(robot_in_a_grid_optimized(grid))
+    g = [[True, True, True, True, True, False],         # - - - - - X
+         [False, False, False, False, True, False],     # X X X X - X
+         [True, True, True, True, True, False],         # X X X X - X
+         [True, False, False, False, True, False],      # X X X X - X
+         [True, True, True, True, True, True]]          # X X X X - -
+    print(robot_in_a_grid(g))
+    print(robot_in_a_grid_optimized(g))
